392c119199c06c993b2836865f671735f73d1a1f
etnite <brian.phillip.ward@gmail.com>
2022-02-16 16:54:48 -0500

    working draft of store_samps.sh

--- code/store_samps.sh
+++ code/store_samps.sh
@@ -6,9 +6,9 @@
 
 #### User-Defined Constants ###########
 
-vcf_file=""
-db_path=""
-chunk_size=1000
+vcf_file="/autofs/bioinformatics-ward/tiledb_test/500_random_Norgrains_samps.bcf"
+db_path="/autofs/bioinformatics-ward/tiledb_test/500samps"
+chunk_size=100
 
 
 #### Executable #######################
@@ -28,7 +28,7 @@ n_samps=$(wc -l < "${samps_tmp}/samps.txt")
 if [[ $n_samps -lt $chunk_size ]]; then
     bcftools +split "$vcf_file" -Ob -o "$ssvcf_tmp"
     for f in "$ssvcf_tmp"/*.bcf; do bcftools index -c "$f"; done
-    #tiledbvcf store --uri "$db_path" "$ssvcf_tmp"/*.bcf
+    tiledbvcf store --uri "$db_path" "$ssvcf_tmp"/*.bcf
 
 ## It's much more involved if the VCF has more samples than the number of files
 ## the system can have open at once. In this case we need to split in two stages
@@ -37,22 +37,23 @@ else
     ## Split up our samples file into chunks. For each we create a 3-col file
     ## with sample name, new sample name (just dash to leave as-is) and the
     ## prefix of the BCF file to output to
-    split -l $chunk_size "${samps_tmp}/samps.txt"
+    split -l $chunk_size "${samps_tmp}/samps.txt" "${samps_tmp}/"
     rm "${samps_tmp}/samps.txt"
     for f in "${samps_tmp}"/*; do
-        awk -v fpref="$f" 'BEGIN {OFS = "\t"}{print $0, "-", fpref}' "${f}_update.txt"
+        base=$(basename "$f")
+        awk -v fpref="$base" 'BEGIN {OFS = "\t"}{print $0, "-", fpref}' "$f" > "${f}_update.txt"
     done
 
     ## Concatenate back together and perform first split
-    cat "${samps_tmp}"/*update.txt > split_key.txt
-    bcftools +split "$vcf_file" -Ob -o "$cvcf_tmp"
+    cat "${samps_tmp}"/*update.txt > "${samps_tmp}/split_key.txt"
+    bcftools +split "$vcf_file" -G "${samps_tmp}/split_key.txt" -Ob -o "$cvcf_tmp"
 
     ## Now loop through each primary split BCF and perform the secondary split
     ## (by individual samples); upload all to the DB
     for f in "$cvcf_tmp"/*.bcf; do
         bcftools +split "$f" -Ob -o "$ssvcf_tmp"
         for g in "$ssvcf_tmp"/*.bcf; do bcftools index -c "$g"; done
-        #tiledbvcf store --uri "$db_path" "$ssvcf_tmp"/*.bcf
+        tiledbvcf store --uri "$db_path" --log-level info "$ssvcf_tmp"/*.bcf
         rm "$ssvcf_tmp"/*
     done
 fi
